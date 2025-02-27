b52d855f62e3cab1bd3ef9cf5c6d79f8ec9e015a
Onnywrite <vlad.mokeev.061611@gmail.com>
2025-02-28 00:45:34 +0300

    fix: sorry for missing PI in calculations

--- include/lab3_2025/BallRadius.hpp
+++ include/lab3_2025/BallRadius.hpp
@@ -5,9 +5,9 @@
 
 // Calculates radius of a ball by its volume.
 inline constexpr double BallRadius(double volume) {
-    // V = 4/3 * R^3 =>
-    // R = cbrt(3/4 * V)
-    return cbrt(0.75 * volume);
+    // V = 4/3 * pi * R^3 =>
+    // R = cbrt(3/4 * pi * V)
+    return cbrt(0.75 * M_PI * volume);
 }
 
 #endif  // BALLRADIUS_HPP
